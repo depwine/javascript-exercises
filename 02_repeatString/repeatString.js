07ee517db2c0d3c7ae3a2664c7eb077c8837168e
Glib Lakeev <glakeev@gmail.com>
2022-06-21 16:39:34 -0400

    Finished questions 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,36 @@
-const repeatString = function() {
+let string;
+let num;
 
-};
+const repeatString = function(string, num) {
+
+    let newString = string;
+
+
+    if (num === 1){
+        return newString;
+
+    } else if (num == 0) {
+
+        newString = ""; 
+        return newString;
+
+    } else if (num < 1){
+
+        newString = "ERROR";
+        return newString;
+
+    } else if (num > 1) {
+
+        for (i = 1 ; i < num; i++){
+            newString += string;   
+   
+        } 
+
+        return newString;  
+
+
+}
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
-};
+    let reverseString;
+    let stringArray = new Array ();
+    
+    stringArray = Array.from(string);
+    let reverseArray = stringArray.reverse();
+    let arrayJoin = reverseArray.join("");    
+
+    return arrayJoin;  
+    
+    };
 
 // Do not edit below this line
 module.exports = reverseString;

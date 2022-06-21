dec52a013b434928b88c37c4fd17950ee02f81fd
Glib Lakeev <glakeev@gmail.com>
2022-06-21 19:13:57 -0400

    Finished fibonacci question, tested successfully though the spec.js test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,52 @@
-const fibonacci = function() {
+let fibNumber;
+let numOne = 1;
+let numTwo = 1;
+let total;
+
+
+const fibonacci = function(fibNumber) {
+
+    fibNumber = parseInt(fibNumber);
+
+    total = 0;
+    numOne = 1;
+    numTwo = 1;
+
+    //get for 1
+    if (fibNumber < 1){
+
+        return "OOPS";
+        
+    } else if (fibNumber === 1){
+
+        return numOne;
+
+    } else if (fibNumber === 2){
+
+        return numTwo;
+
+    } else if (fibNumber > 1){
+
+        //fib seq
+
+        for (i = 2; i < fibNumber; i++){
+
+            total = numOne + numTwo;
+
+            numOne = numTwo;
+            numTwo = total;
+
+
+        }
+
+        return total;
+
+    }
+
+
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;

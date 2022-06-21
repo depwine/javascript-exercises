ef356a052d1b7bd018f8dd9540d87a77701a2332
Glib Lakeev <glakeev@gmail.com>
2022-06-21 18:36:32 -0400

    finished the palindromes exercise, all vetted through the Spec file

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,71 @@
-const palindromes = function () {
+let string;
+let noSpace;
+let stringArr = new Array();
+let stringRevArr = new Array();
+let compare = true;
+
+const palindromes = function (string) {
+
+
+//get rid of any white space
+string = string.replace(/\s+/g, "");
+
+//replace special characters
+string = string.replace(/[^a-zA-Z ]/g, "");
+
+// upper case it
+string = string.toUpperCase();
+
+//cycle through letters
+
+for (i = 0; i < string.length; i++){
+
+//if space
+
+    if (string.charAt(i) === ""){
+        console.log("has spaces");
+    }
+
+}
+
+//if string 1 word:
+stringArr = string.split("");
+stringRevArr = string.split("");
+
+
+
+
+
+//create reverse string
+stringRevArr = stringRevArr.reverse();
+
+//list both
+console.log(stringArr);
+console.log(stringRevArr);
+
+//join letter for easy compare
+
+stringArr = stringArr.join("");
+stringRevArr = stringRevArr.join("");
+
+//check both
+console.log(stringArr);
+console.log(stringRevArr);
+
+//compare new joined strings.
+
+if (stringArr === stringRevArr){
+
+    compare = true;
+
+} else {
+
+    compare = false;
+
+}
+
+
+return compare;
 
 };
 

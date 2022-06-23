849242905c338bb7fcfc4d98ebe4cb37841eeb50
Glib Lakeev <glakeev@gmail.com>
2022-06-22 21:12:23 -0400

    completed #11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
+const getTheTitles = function(inputArray) {
+
+
+let titleOne = inputArray[0].title;
+let titleTwo = inputArray[1].title;
+
+let titleArray = new Array();
+
+titleArray.push(titleOne, titleTwo);
+
+return titleArray;
 
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
